368336350b92b91bfb4e33611c940af812f473e3
AveryCS <Averydcs@gmail.com>
2022-03-29 10:26:37 -0400

    first commit

---
+++ Backend/SeedAndFeed/src/main/java/com/wcci/SeedAndFeed/SeedAndFeedApplication.java
@@ -0,0 +1,13 @@
+package com.wcci.SeedAndFeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedAndFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedAndFeedApplication.class, args);
+	}
+
+}
---
+++ Backend/SeedAndFeed/src/test/java/com/wcci/SeedAndFeed/SeedAndFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcci.SeedAndFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeedAndFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
