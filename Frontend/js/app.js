4b8dcec0a645c3c398116274b01e9f842c0e8ad4
benjamin-gibbs-2 <benjamin.gibbs2@gmail.com>
2022-04-01 15:19:06 -0400

    APIs Functioning
    All APIs are taking and passing information correctly.

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,6 +1,49 @@
 import home from "./home.js";
 
+// ***************************************************************
+// ***************************************************************
+// THIS IS THE SECTION on HOW TO TAKE ZIPCODE TO FROSTDATE:
+// ***************************************************************
+// THE FUNCTION " findFrostDateFromZipCode " IS RUN IN  makeHomeViewFromJSON
+//***************************************************************
 
+function findFrostDateFromZipCode() {
+  const zipNumb = document.querySelector(".zipcode");
+  console.log(zipNumb.value);
+  fetch(`https://phzmapi.org/${zipNumb.value}.json`
+  )
+    .then((res) => {
+      console.log(res);
+      return res.json();
+    })
+    .then((zipDetails) => {
+      console.log(zipDetails);
+
+      fetch(
+        `https://api.farmsense.net/v1/frostdates/stations/?lat=${zipDetails.coordinates.lat}&lon=${zipDetails.coordinates.lon}`
+      )
+        .then((res) => res.json())
+        .then((stationID) => {
+          stationID;
+
+          fetch(
+            `https://api.farmsense.net/v1/frostdates/probabilities/?station=${stationID[0].id}&season=1`
+          )
+            .then((res) => res.json())
+            .then((frostDateNumb) => {
+              frostDateNumb[0].prob_50;
+              console.log(frostDateNumb[0].prob_50);
+              let fdate = frostDateNumb[0].prob_50;
+              console.log("F-DATE NEXT")
+              console.log(fdate);
+
+            });
+        });
+    })
+    .catch((error) => console.error(error));
+}
+// -----------------------------------------------------------------------------
+// ***************************************************************
 
 
 // *************************************************
@@ -8,55 +51,53 @@ import home from "./home.js";
 // THIS IS THE HOMEVIEW SECTION:
 // *************************************************
 const containerEl = document.querySelector(".container");
-function makeHomeView(){
-    console.log("working");
-    fetch("http://localhost:8080/")
+function makeHomeView() {
+  console.log("working");
+  fetch("http://localhost:8080/")
     .then((res) => res.json())
     .then((plants) => {
-        makeHomeViewFromJSON(plants);
-    })
-    
-
+      makeHomeViewFromJSON(plants);
+    });
 }
 
-function makeHomeViewFromJSON(plants){
+function makeHomeViewFromJSON(plants) {
   containerEl.innerHTML = home(plants);
   //bind function will go here
   const checkBoxDivs = containerEl.querySelector(".veg_id");
 
-    const submit_button = document.querySelector(".submitButton");
-
+  const submit_button = document.querySelector(".submitButton");
 
   submit_button.addEventListener("click", ValidateVegSelection);
+
+  const zipSubBtn = containerEl.querySelector(".zipSubmitButton");
+  zipSubBtn.addEventListener("click", findFrostDateFromZipCode);
+
   console.log(plants.id);
 }
-   
+
 makeHomeView();
 
+function ValidateVegSelection() {
+  var array = [];
+  var queryString = "";
+  var checkboxes = document.querySelectorAll("input:checked");
+  // console.log(checkboxes)
+  for (var i = 0; i < checkboxes.length; i++) {
+    console.log(checkboxes[i].parentElement.querySelector(".veg_id").value);
+    array.push(checkboxes[i].parentElement.querySelector(".veg_id").value);
+    queryString +=
+      checkboxes[i].parentElement.querySelector(".veg_id").value + ",";
+    // TRIM QUERYSTRING HERE AS STRETCH GOAL
 
-
-function ValidateVegSelection()  { 
-
-
-var array = [];
-var queryString = "";
-var checkboxes = document.querySelectorAll("input:checked");
-// console.log(checkboxes)
-for (var i = 0; i < checkboxes.length; i++) {
-  console.log(checkboxes[i].parentElement.querySelector(".veg_id").value);
-  array.push(checkboxes[i].parentElement.querySelector(".veg_id").value);
-  queryString +=
-    checkboxes[i].parentElement.querySelector(".veg_id").value + "," ;
-  // TRIM QUERYSTRING HERE AS STRETCH GOAL
-
-
-console.log(queryString);
+    console.log(queryString);
+  }
+  console.log(queryString);
+  console.log("stop reloading");
+  window.location.href =
+    "http://localhost:8080/plants?plantsIds=" + queryString;
 }
-console.log(queryString);
-console.log("stop reloading")
-window.location.href = "http://localhost:8080/plants?plantsIds="+queryString;
-}  
-
+// -----------------------------------------------------------------------------
+// ***************************************************************
 
 
 
@@ -68,26 +109,25 @@ window.location.href = "http://localhost:8080/plants?plantsIds="+queryString;
 // *************************************************
 
 
+// SINGLE PLAT CODE HERE:
+
+// -----------------------------------------------------------------------------
+// ***************************************************************
+
+
+
 
 // *************************************************
 // **************************************************
 // THIS IS THE SINGLE PLANT RECIPES VIEW SECTION:
 // *************************************************
 
-
-
-
 // *************************************************
 // **************************************************
 // THIS IS THE SINGLE RECIPE VIEW SECTION, FROM PLANT:
 // *************************************************
 
-
-
-
-
 // *************************************************
 // **************************************************
 // THIS IS THE ALL RECIPE VIEW SECTION:
 // *************************************************
-
