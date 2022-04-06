dabf699c9dcf59d62e93f68fc55f07db556cbea1
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-04-06 13:43:07 -0400

    added About Us modal

---
+++ Frontend/js/aboutView.js
@@ -0,0 +1,26 @@
+export default function aboutView () {
+    return `
+    <h1>About the Developers</h1>
+    <div class="aboutContent">
+    <h2>Avery Smith</h2>
+    <p>About section Avery</p>
+    <p>Add in Github, LinkedIn, portfolio logos with links here</p>
+    <br>
+    <h2>Ryan McCurdy</h2>
+    <p>About section Ryan</p>
+    <p>Add in Github, LinkedIn, portfolio logos with links here</p>
+    <br>
+    <h2>Ben Gibbs</h2>
+    <p>About section Ben</p>
+    <p>Add in Github, LinkedIn, portfolio logos with links here</p>
+    <br>
+    <h2>Jennifer Adams</h2>
+    <p>About section Jennifer</p>
+    <p>Add in Github, LinkedIn, portfolio logos with links here</p>
+    <br>
+    <h2>Dylan Creech</h2>
+    <p>About section Dylan</p>
+    <p>Add in Github, LinkedIn, portfolio logos with links here</p>
+    </div>
+    `
+}
