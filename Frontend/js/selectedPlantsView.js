39ebaf1c0585878191a9edd58c6ea34e9f09dd06
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-04-12 11:55:51 -0400

    styling for selected plants view

--- Frontend/js/selectedPlantsView.js
+++ Frontend/js/selectedPlantsView.js
@@ -8,40 +8,54 @@ export default function selectedPlantsView(plants, d, stationName){
     console.log(plants + d);
     const lengthOfDay = (24 * 60 * 60 * 1000);
 return `
-<button class ="backBtn">Back Button</button>
-<button onclick="window.print()" class="printBtn">Print this Page</button>
+
+<div class="backButton">
+<img class ="backBtn" src="/img/back_arrow.png">
+</div>
+<div class="frostDate">
+<h2>Your average last frost date is ${d.toDateString()}</h2>
+</div>
+
 <div class="plantInfo">
-<h2>This is the average last frost date for your area + ${d.toDateString()}</h2>
-
-
 ${plants
   .map((plant) => {
     return `
     <div class="singlePlant">
     <input type="hidden" class="plantId" value="${plant.id}">
-<h1 class="plantName">${plant.name}</h1>
+<div class="plantNameAndImg">
+    <h2 class="plantName">${plant.name}</h2>
 <img class="plantPhotoSingle" src=${plant.photoLink}>
-<p>${plant.careTips}</p>
+</div>
+<div class="careTips">
+<h3>${plant.careTips}</h3>
+</div>
+
+<div class="plantDetails">
 <h3 class="maturityDay"> Days to maturity: ${plant.maturityDays}</h3>
 <h3 class="height"> Height: ${plant.height} inches</h3>
 <h3 class="sun"> Sun exposure: ${plant.sunExposure}</h3>
-<h3 class="companion">Recommended Companion Plants: ${plant.companionPlants}</h3>
+<h3>Indoor start date: ${indoorCalcFrag(plant, d)}</h3>
+<h3>Outdoor start date: ${d.toDateString()}</h3>
+</div>
 
+<div class="companionDiv">
+<p class="companion">Recommended Companion Plants: ${plant.companionPlants}</p>
+</div>
 
+<div class="plantRecButton">
+<button class ="clickForRecipeButton">${plant.name} recipes </button>
+</div>
 
-
-<button class ="clickForRecipeButton">Click for ${plant.name} recipes </button>
-
-<h1>Recommended indoor start date: ${indoorCalcFrag(plant, d)}</h1>
-<h1>Recommended outdoor start date: ${d.toDateString()}</h1>
+<div class="plantCalBtn">
 <button id="default-button">Calendar Button${calendarButton(indoorStartFrag(plant, d), plant, d)}</button>
+</div>
 
 </div>`;
   })
   .join("")}
 
   <h2 class="stationDisplay">Nearest Reporting Weather Station: ${((stationName.toLowerCase(stationName)))}</h2>
-  
+  <button onclick="window.print()" class="printBtn">Print this Page</button>
 </div>`;
 
 
