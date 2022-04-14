3e8d121e8460f5db50569eb2d377f10de702854f
jenniferadamsphoto <jenniferadamsphoto@gmail.com>
2022-04-14 01:07:19 -0400

    styling for home, selected, about, resources pages

--- Frontend/js/selectedPlantsView.js
+++ Frontend/js/selectedPlantsView.js
@@ -31,13 +31,8 @@ ${plants
     <h2 class="plantName">${plant.name}</h2>
 <img class="plantPhotoSingle" src=${plant.photoLink}>
 </div>
-<div class="careTips">
-<h2>Care Tips:</h2>
-<p class="careTipsWords">${plant.careTips}</p>
-</div>
-
 <div class="plantDetails">
-<h2>Plant Info:</h2>
+<h2 class="infoWords">Plant Info:</h2>
 <h3 class="maturityDay"> Days to maturity: ${plant.maturityDays}</h3>
 <h3 class="height"> Height: ${plant.height} inches</h3>
 <h3 class="sun"> Sun exposure: ${plant.sunExposure}</h3>
@@ -45,14 +40,20 @@ ${plants
 <h3>Outdoor start date: ${d.toDateString()}</h3>
 </div>
 
+<div class="careTips">
+<h2 class="careTitle">Care Tips:</h2>
+<p class="careTipsWords">${plant.careTips}</p>
+</div>
+
+
+
 <div class="companionDiv">
-<p class="companion">Recommended Companion Plants: ${plant.companionPlants}</p>
+<h2 class="compTitle">Companion Plants:</h2>
+<p class="companion">${plant.companionPlants}</p>
 </div>
 
 <div class="plantRecButton">
 <button class ="clickForRecipeButton">${plant.name} recipes </button>
-</div>
-
 <div class="plantCalBtn">
 <button id="default-button">${calendarButton(
       indoorStartFrag(plant, d),
@@ -60,11 +61,14 @@ ${plants
       d
     )}</button>
 </div>
+</div>
+
+
 
 </div>`;
   })
   .join("")}
-
+</div>
 <div class="footer_documentation_link">
   <p>API resources used on this page: <a href="https://www.phzmapi.org">phzmapi.org</a> and <a href="https://www.farmsense.net">farmsense.net</a></p>
 </div>
